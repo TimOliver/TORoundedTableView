314babecc739c6209a78b06750c60455c1698640
Tim Oliver <me@timoliver.com.au>
2017-04-15 11:45:33 -0700

    Fixed typo in README

---
+++ TORoundedTableViewExample/SplitViewController.h
@@ -0,0 +1,13 @@
+//
+//  SplitViewController.h
+//  TORoundedTableView
+//
+//  Created by Tim Oliver on 3/26/17.
+//  Copyright © 2017 Tim Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SplitViewController : UISplitViewController
+
+@end
---
+++ TORoundedTableViewExample/SplitViewController.m
@@ -0,0 +1,37 @@
+//
+//  SplitViewController.m
+//  TORoundedTableView
+//
+//  Created by Tim Oliver on 3/26/17.
+//  Copyright © 2017 Tim Oliver. All rights reserved.
+//
+
+#import "SplitViewController.h"
+
+@interface SplitViewController ()
+
+@end
+
+@implementation SplitViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
